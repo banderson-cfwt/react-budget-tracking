4a20c7065a44e87e0d20055b3734bab39cf90335
Bryan Anderson <bryan.anderson@cfwebtools.com>
2022-08-15 13:53:44 -0400

    redo app structure

--- src/App.js
+++ src/App.js
@@ -1,35 +1,70 @@
-import React, { useRef } from 'react';
-import Main from './components/main/Main';
-import Details from './components/Details/Details';
-import { PushToTalkButton, PushToTalkButtonContainer } from '@speechly/react-ui';
-import { Grid } from '@material-ui/core';
-import useStyles from './styles';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorMapping, ColorPicker, Editor } from './pages';
+
+import './App.css';
 
 const App = () => {
-  const classes = useStyles();
-  const main = useRef(null)
+  const activeMenu = true;
 
   return (
     <div>
-      <Grid className={classes.grid} container spacing={0} alignItems="center" justifyContent="center" style={{ height: '100vh'}}>
-        <Grid item xs={12} sm={4} className={classes.mobile}>
-          <Details title="Income" />
-        </Grid>
-        <Grid ref={main} item xs={12} sm={3} className={classes.main}>
-          <Main />
-        </Grid>
-        <Grid item xs={12} sm={4} className={classes.desktop}>
-          <Details title="Income" />
-        </Grid>
-        <Grid item xs={12} sm={4} className={classes.last}>
-          <Details title="Expense" />
-        </Grid>
-        <PushToTalkButtonContainer>
-          <PushToTalkButton />
-        </PushToTalkButtonContainer>
-      </Grid>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' style={{ background: 'blue', borderRadius: '50%' }}>
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              <Sidebar />
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
+          )}
+          <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              <Navbar />
+            </div>
+          </div>
+
+          <div>
+            <Routes>              
+              <Route path="/" element={<Ecommerce />} />
+              <Route path="/ecommerce" element={<Ecommerce />} />
+
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/employees" element={<Employees />} />
+              <Route path="/customers" element={<Customers />} />
+
+              <Route path="/kanban" element={<Kanban />} />
+              <Route path="/editor" element={<Editor />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/color-picker" element={<ColorPicker />} />
+
+              <Route path="/line" element={<Line />} />
+              <Route path="/area" element={<Area />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/financial" element={<Financial />} />
+              <Route path="/color-mapping" element={<ColorMapping />} />
+              <Route path="/pyramid" element={<Pyramid />} />
+              <Route path="/stacked" element={<Stacked />} />
+            </Routes>
+          </div>
+
+        </div>
+      </BrowserRouter>
     </div>
-  );
-};
+  )
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { SpeechProvider } from "@speechly/react-client";
-import { Provider } from './context/context';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <SpeechProvider appId="a74ea097-ac46-4f44-90b6-141c61fa4620" language="en-US">
-    <Provider>
-      <App />
-    </Provider>
-  </SpeechProvider>, 
-  document.getElementById('root')
+  <App />,
+  document.getElementById('root'),
 );
