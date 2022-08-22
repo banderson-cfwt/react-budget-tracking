010a103d0c936ba22e9e1598f91151c4f5c5ada7
Bryan Anderson <bryan.anderson@cfwebtools.com>
2022-08-22 16:39:45 -0400

    refactored the app layout

--- src/App.js
+++ src/App.js
@@ -1,93 +1,10 @@
-import React, { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { FiSettings } from 'react-icons/fi';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
-
-import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
-import { Dashboard, Orders, Employees, Stacked, Pyramid, Customers, Line, Area, Bar, Pie, Financial, ColorMapping, Login } from './pages';
+import React from 'react';
+import Layout from './pages/Layout';
 import './App.css';
 
-import { useStateContext } from './contexts/ContextProvider';
-
 const App = () => {
-  const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
-
-  useEffect(() => {
-    const currentThemeColor = localStorage.getItem('colorMode');
-    const currentThemeMode = localStorage.getItem('themeMode');
-    if (currentThemeColor && currentThemeMode) {
-      setCurrentColor(currentThemeColor);
-      setCurrentMode(currentThemeMode);
-    }
-  }, []);
-
   return (
-    <div className={currentMode === 'Dark' ? 'dark' : ''}>
-      <div className="relative flex dark:bg-main-dark-bg">
-        <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
-          <TooltipComponent
-            content="Settings"
-            position="Top"
-          >
-            <button
-              type="button"
-              onClick={() => setThemeSettings(true)}
-              style={{ background: currentColor, borderRadius: '50%' }}
-              className="p-3 text-3xl text-white hover:drop-shadow-xl hover:bg-light-gray"
-            >
-              <FiSettings />
-            </button>
-
-          </TooltipComponent>
-        </div>
-        {activeMenu ? (
-          <div className="fixed bg-white w-72 sidebar dark:bg-secondary-dark-bg ">
-            <Sidebar />
-          </div>
-        ) : (
-          <div className="w-0 dark:bg-secondary-dark-bg">
-            <Sidebar />
-          </div>
-        )}
-        <div
-          className={
-            activeMenu
-              ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
-              : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
-          }
-        >
-          <div className="fixed w-full md:static bg-main-bg dark:bg-main-dark-bg navbar ">
-            <Navbar />
-          </div>
-          <div>
-            {themeSettings && (<ThemeSettings />)}
-
-            <Routes>
-              {/* dashboard  */}
-              <Route path="/" element={(<Dashboard />)} />
-              <Route path="/dashboard" element={(<Dashboard />)} />
-
-              {/* pages  */}
-              <Route path="/orders" element={<Orders />} />
-              <Route path="/employees" element={<Employees />} />
-              <Route path="/customers" element={<Customers />} />
-
-              {/* charts  */}
-              <Route path="/line" element={<Line />} />
-              <Route path="/area" element={<Area />} />
-              <Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/financial" element={<Financial />} />
-              <Route path="/color-mapping" element={<ColorMapping />} />
-              <Route path="/pyramid" element={<Pyramid />} />
-              <Route path="/stacked" element={<Stacked />} />
-
-            </Routes>
-          </div>
-          <Footer />
-        </div>
-      </div>
-    </div>
+    <Layout />
   );
 };
 
